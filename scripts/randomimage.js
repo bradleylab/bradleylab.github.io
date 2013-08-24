0ca6329e870defe91de87cc9d9082c57804b506b
bradleylab <bradleylab@gmail.com>
2013-08-24 11:58:53 -0500

    cleaned up js. removed .scrollable css

--- scripts/randomimage.js
+++ scripts/randomimage.js
@@ -2,48 +2,29 @@
 	
 	$.randomImage = {
 		defaults: {
-			
-			//you can change these defaults to your own preferences.
-			path: '/images/', //change this to the path of your images
-			myImages: ['autosampler.jpg', 'flasks.jpg', 'flatcone.jpg', 'gcmsdata.jpg', 'horxcoring.jpg', 'labbench.jpg', 'srpathway.jpg', 'tropicalplants.jpg' ] //put image names in this bracket. ex: 'harold.jpg', 'maude.jpg', 'etc'
-			
+			path: '/images/', //path to images
+			myImages: ['autosampler.jpg', 'flasks.jpg', 'flatcone.jpg', 'gcmsdata.jpg', 'horxcoring.jpg', 'labbench.jpg', 'srpathway.jpg', 'tropicalplants.jpg' ] //image names here
 		}			
 	}
 	
 	$.fn.extend({
 			randomImage:function(config) {
-				
 				var config = $.extend({}, $.randomImage.defaults, config); 
-				
-				 return this.each(function() {
-						
-						var imageNames = config.myImages;
-						
+				 return this.each(function() {						
+						var imageNames = config.myImages;						
 						//get size of array, randomize a number from this
 						// use this number as the array index
-
 						var imageNamesSize = imageNames.length;
-
 						var lotteryNumber = Math.floor(Math.random()*imageNamesSize);
-
 						var winnerImage = imageNames[lotteryNumber];
-
 						var fullPath = config.path + winnerImage;
-						
-						
 						//put this image into DOM at class of randomImage
 						// alt tag will be image filename.
 						$(this).attr( {
 										src: fullPath,
 										alt: winnerImage
 									});
-				
-						
 				});	
 			}
-			
 	});
-	
-	
-	
 })(jQuery);
